72b993d53d6f55e36d91c5965e5673866ab4c3c1
Curd Becker <me@curd-becker.de>
2024-08-23 00:00:00 +0200

    SDK RUTX_R_GPL_00.07.08.3

--- target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -11,6 +11,14 @@ platform_do_upgrade() {
 	platform_do_upgrade_ipq "$1"
 }
 
+compatible_to_eg060k() {
+	{ ! find_hw_mod "EG060K"; } && {
+		echo "EG060K modem detected but fw does not support it"
+		return 1
+	}
+	return 0
+}
+
 prepare_metadata_hw_supp () {
 	local metadata="/tmp/sysupgrade.meta"
 
@@ -37,35 +45,42 @@ platform_check_hw_support() {
 	local io_expander_file="/proc/device-tree/io_expander"
 	local nand_model_file="/sys/class/mtd/mtd16/nand_model"
 
-	if prepare_metadata_hw_mods "$1"; then
-		# nand type validation
-		grep -q '^W25N02KV' "$nand_model_file" && { ! find_hw_mod "W25N02KV"; } && {
-			echo "Winbond NAND detected but fw does not support it"
-			return 1
-		}
-		grep -q '^GD5F2GM7\|^GD5F2GQ5' "$nand_model_file" && { ! find_hw_mod "NAND_GD5F2GXX"; } && {
-			echo "GigaDevices NAND detected but fw does not support it"
+	{ !  prepare_metadata_hw_supp "$1"; } && return 1
+
+	# io_expander type validation
+	if grep -q '^shiftreg_1$' "$io_expander_file"; then
+		{ ! find_io_exp "shiftreg_1"; } && {
+			echo "Shift registers detected but fw does not support it"
 			return 1
 		}
 	else
-		return 1
+		# fail if not default/initial type
+		grep -q '^stm32$' "$io_expander_file" || return 1
 	fi
 
-	if prepare_metadata_hw_supp "$1"; then
-		# io_expander type validation
-		if grep -q '^shiftreg_1$' "$io_expander_file"; then
-			{ ! find_io_exp "shiftreg_1"; } && {
-				echo "Shift registers detected but fw does not support it"
-				return 1
-			}
-		else
-			# fail if not default/initial type
-			grep -q '^stm32$' "$io_expander_file" || return 1
-		fi
-	else
-		return 1
-	fi
+	{ ! prepare_metadata_hw_mods "$1"; } && return 1
 
-	return 0
+	# nand type validation
+	grep -q '^W25N02KV' "$nand_model_file" && { ! find_hw_mod "W25N02KV"; } && {
+		echo "Winbond NAND detected but fw does not support it"
+		return 1
+	}
+	grep -q '^GD5F2GM7\|^GD5F2GQ5' "$nand_model_file" && { ! find_hw_mod "NAND_GD5F2GXX"; } && {
+		echo "GigaDevices NAND detected but fw does not support it"
+		return 1
+	}
+
+	eval "$( jsonfilter -q -i "/etc/board.json" \
+		-e "vendor=@['modems'][@.builtin=true].vendor" \
+		-e "product=@['modems'][@.builtin=true].product" \
+		)"
+
+	# device without modem
+	[ -z "$vendor" ] || [ -z "$product" ] && return 0
+
+	[ "${vendor}:${product}" != "2c7c:030b" ] && return 0
+
+	compatible_to_eg060k
+
+	return "$?"
 }
-
